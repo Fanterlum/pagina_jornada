465df3ad823dca2f059598b81173a0ee7a490619
Lalo Martinez <109983168+MMTZ1011@users.noreply.github.com>
2023-03-21 15:01:36 -0600

    Calendario Home terminado
    - se añadio la base diasdatos.js
    - el calendario ya funciona y carga  los datos

---
+++ js/DiasDatos.js
@@ -0,0 +1,410 @@
+
+
+const DiasDatos = {
+    1: { // DIA 1
+        IDTAg: "Abril17",
+        Fecha: "ABRIL 17",
+        Dia: "LUNES",
+        Dia_num: 17,
+        Eventos: {
+            
+            1: {
+                Desde: "15:00",
+                Hasta: "17:00",
+                Tema: "Inaguración",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: null ,
+                
+            },
+            
+            
+        }//Eventos
+    }, //DEL DIA 1
+
+    2: { // DIA 2
+        IDTAg: "Abril18",
+        Fecha: "ABRIL 18",
+        Dia: "MARTES",
+        Dia_num: 18,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "13:00",
+                Tema: "Propiedad intelectual y Emprendimiento digital",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Rodolfo Guerrero" ,
+                
+            },
+            2: {
+                Desde: "17:00",
+                Hasta: "19:00",
+                Tema: "[Tema Pendiente]",
+                Subtema: null,
+                Lugar: "Auditorio Y",
+                Ponente: "Manuel Huescas" ,
+            },
+            
+            
+        }//Eventos
+    },//DEL DIA 2
+
+    3: { // DIA 3
+        IDTAg: "Abril19",
+        Fecha: "ABRIL 19",
+        Dia: "MIERCOLES",
+        Dia_num: 19,
+        Eventos: {
+            1: {
+                Desde: "15:00",
+                Hasta: "16:00",
+                Tema: "Machine Learning aplicado a los negocios e innovación",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Kevin Torres" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 3
+
+    4: { // DIA 4
+        IDTAg: "Abril20",
+        Fecha: "ABRIL 20",
+        Dia: "JUEVES",
+        Dia_num: 20,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "13:00",
+                Tema: "Gamificar para impulsar tu emprendimiento",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Fernando Ramirez" ,
+            },
+            2: {
+                Desde: "17:00",
+                Hasta: "19:00",
+                Tema: "Curso Impresión 3D",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Jesús Bonilla" ,
+            },
+            3: {
+                Desde: "17:00",
+                Hasta: "19:00",
+                Tema: "Validación 101",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Angel Gonzalez Román" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 4
+
+    5: { // DIA 5
+        IDTAg: "Abril21",
+        Fecha: "ABRIL 21",
+        Dia: "VIERNES",
+        Dia_num: 21,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "15:00",
+                Tema: "Video Juegos",
+                Subtema: "Concurso de videojuegos",
+                Lugar: "Auditorio",
+                Ponente: "" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 5
+
+    ////////////////////////SEMANA 2
+    6: { // DIA 6
+        IDTAg: "Abril24",
+        Fecha: "ABRIL 24",
+        Dia: "LUNES",
+        Dia_num: 24,
+        Eventos: {
+            1: {
+                Desde: "15:00",
+                Hasta: "16:00",
+                Tema: "Machine Learning aplicado a los negocios e innovación",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Adriana zambrazo" ,
+            },
+            2: {
+                Desde: "14:00",
+                Hasta: "15:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "DR LUQUE VEGA" ,
+            },
+            3: {
+                Desde: "14:00",
+                Hasta: "15:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "DR LUQUE VEGA" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 6
+
+    7: { // DIA 7
+        IDTAg: "Abril25",
+        Fecha: "ABRIL 25",
+        Dia: "MARTES",
+        Dia_num: 25,
+        Eventos: {
+            
+            
+        }//Eventos
+    },//DEL DIA 7
+
+    8: { // DIA 8
+        IDTAg: "Abril26",
+        Fecha: "ABRIL 26",
+        Dia: "MIERCOLES",
+        Dia_num: 26,
+        Eventos: {
+        
+            
+        }//Eventos
+    },//DEL DIA 8
+
+    9: { // DIA 9
+        IDTAg: "Abril27",
+        Fecha: "ABRIL 27",
+        Dia: "JUEVES",
+        Dia_num: 27,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "12:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Dra. Rocio" ,
+            },
+            2: {
+                Desde: "12:00",
+                Hasta: "13:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Dra. Rocio" ,
+            },
+            3: {
+                Desde: "17:00",
+                Hasta: "19:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Tlaloc" ,
+            },
+            
+        }//Eventos
+    },//DEL DIA 9
+
+    10: { // DIA 10
+        IDTAg: "Abril28",
+        Fecha: "ABRIL 28",
+        Dia: "VIERNES",
+        Dia_num: 28,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "15:00",
+                Tema: "Video Juegos",
+                Subtema: "Concurso de videojuegos",
+                Lugar: "Auditorio",
+                Ponente: "" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 10
+
+    11: { // DIA 11
+        IDTAg: "Mayo1",
+        Fecha: "MAYO 1",
+        Dia: "LUNES",
+        Dia_num: 1,
+        Eventos: {
+            
+            /*
+            NO HAY NADA ESTE DÍA
+            */
+            
+        }//Eventos
+    },//DEL DIA 11
+
+    12: { // DIA 12
+        IDTAg: "Mayo2",
+        Fecha: "MAYO 2",
+        Dia: "MARTES",
+        Dia_num: 2,
+        Eventos: {
+            1: {
+                Desde: "15:00",
+                Hasta: "16:00",
+                Tema: "Primer aproach. Desarrollo de habilidades blandas para el emprendimiento",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Itzgal Tenorio" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 12
+
+    13: { // DIA 13
+        IDTAg: "Mayo3",
+        Fecha: "MAYO 3",
+        Dia: "MIERCOLES",
+        Dia_num: 3,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "13:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Dra. Aimee Esparza " ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 13
+
+    14: { // DIA 14
+        IDTAg: "Mayo4",
+        Fecha: "MAYO 4",
+        Dia: "JUEVES",
+        Dia_num: 4,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "13:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Abraham Arce" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 14
+
+    15: { // DIA 15
+        IDTAg: "Mayo5",
+        Fecha: "MAYO 5",
+        Dia: "VIERNES",
+        Dia_num: 5,
+        Eventos: {
+            1: {
+                Desde: "10:00",
+                Hasta: "11:00",
+                Tema: "Marinela",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "" ,
+            },
+            2: {
+                Desde: "11:00",
+                Hasta: "17:00",
+                Tema: "Talent",
+                Subtema: "Concurso de talentos",
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: null ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 15
+
+    16: { // DIA 16
+        IDTAg: "Mayo8",
+        Fecha: "MAYO 8",
+        Dia: "LUNES",
+        Dia_num: 8,
+        Eventos: {
+            1: {
+                Desde: "09:00",
+                Hasta: "12:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "Dr. Mario" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 16
+
+    17: { // DIA 17
+        IDTAg: "Mayo9",
+        Fecha: "MAYO 9",
+        Dia: "MARTES",
+        Dia_num: 9,
+        Eventos: {
+            1: {
+                Desde: "17:00",
+                Hasta: "19:00",
+                Tema: "[Pendiente]",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: "MarUsia oL virtud" ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 17
+
+    18: { // DIA 18
+        IDTAg: "Mayo10",
+        Fecha: "MAYO 10",
+        Dia: "MIERCOLES",
+        Dia_num: 10,
+        Eventos: {
+            
+            
+        }//Eventos
+    },//DEL DIA 18
+
+    19: { // DIA 19
+        IDTAg: "Mayo11",
+        Fecha: "MAYO 11",
+        Dia: "JUEVES",
+        Dia_num: 11,
+        Eventos: {
+            1: {
+                Desde: "17:00",
+                Hasta: "18:00",
+                Tema: "Jmd",
+                Subtema: null,
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: null ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 19
+
+    20: { // DIA 20
+        IDTAg: "Mayo12",
+        Fecha: "MAYO 12",
+        Dia: "VIERNES",
+        Dia_num: 12,
+        Eventos: {
+            1: {
+                Desde: "11:00",
+                Hasta: "15:00",
+                Tema: "Final de Videojuegos",
+                Subtema: "Concurso de videojuegos",
+                Lugar: ["Auditorio", "🖥️ Online"],
+                Ponente: null ,
+            }
+            
+        }//Eventos
+    },//DEL DIA 20
+    
+
+};//cosas
